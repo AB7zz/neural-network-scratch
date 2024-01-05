ea62ffa71aaf2f01ef36d0a08a06b7aa932df855
AB7zz <abhinavcv007@gmail.com>
2024-01-05 18:21:12 +0530

    code

--- app.py
+++ app.py
@@ -2,6 +2,96 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('/kaggle/input/digit-recognizer/train.csv')
+data = pd.read_csv('train.csv')
 
-data.head()
+data = np.array(data)
+m, n = data.shape
+# np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+def init_params():
+    W1 = np.random.rand(10, 784)
+    b1 = np.random.rand(10, 1) - 0.5
+
+    W2 = np.random.rand(10, 10)
+    b2 = np.random.rand(10, 1) - 0.5
+
+    return W1, b1, W2, b2
+
+def ReLU(Z):
+    return np.maximum(0, Z)
+
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(A1)
+
+    return Z1, A2, Z2, A2
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def derivReLU(Z):
+    return Z > 0
+
+def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    m = Y.size()
+    one_hot_Y = one_hot(Y)
+
+    dZ2 = A2 - one_hot_Y
+    dW2 = (1 / m) * dZ2.dot(A1.T)
+    db2 = (1 / m) * np.sum(dZ2, 2)
+    
+    dZ1 = W2.T.dot(dZ2) * derivReLU(Z1)
+    dW1 = (1 / m) * dZ1.dot(X.T)
+    db1 = (1 / m) * np.sum(dZ1, 2)
+     
+    return dW1, db1, dW2, db2
+
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+
+    return W1, b1, W2, b2
+
+def get_accuracy(predictions, Y):
+    print(predictions)
+    print(Y)
+    return np.sum(predictions == Y) / Y.size
+
+def get_predictions(W1, b1, W2, b2, X):
+    Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = np.argmax(A2, 0)
+    return predictions
+
+def gradient_descent(X, Y, iterations, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            print("Accuracy: ", get_accuracy(get_predictions(W1, b1, W2, b2, X), Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 100, 0.1)
